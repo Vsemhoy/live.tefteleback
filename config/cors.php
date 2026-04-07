028065d33225f02d489ad9be0a8c718831ee748c
TefteleCom <77360455+Vsemhoy@users.noreply.github.com>
2026-04-07 12:53:43 +0300

    up

--- config/cors.php
+++ config/cors.php
@@ -1,8 +1,9 @@
 <?php
+
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'], // или ['GET','POST','OPTIONS'] и др.
-    'allowed_origins' => ['https://okfront.okkio.ru'], // обязательно точное значение
+    'allowed_origins' => ['https://teftele.com', 'https://www.teftele.com'], // обязательно точное значение
     'allowed_headers' => ['*'],
-    'supports_credentials' => false, // если нужны куки/авторизация
+    'supports_credentials' => true, // если нужны куки/авторизация
 ];
