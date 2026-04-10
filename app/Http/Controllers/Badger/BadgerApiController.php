87435a7a7a88d24f98d7d85cd927054cb69ddba2
TefteleCom <vsemhoy@live.ru>
2026-04-11 00:46:35 +0300

    up

---
+++ app/Http/Controllers/Badger/BadgerApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Eventor;
+
+use App\Http\Controllers\Controller;
+use App\Models\EvtCategory;
+use App\Models\EvtEvent;
+use App\Models\EvtSection;
+use App\Models\EvtTag;
+use App\Models\EvtType;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Symfony\Component\Uid\Ulid;
+
+class BadgerApiController extends Controller {
+
+
+
+}
